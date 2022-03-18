f037df4d49a857faca83f9df8517814077ad9ff9
Chris Culnane <chrisculnane@gmail.com>
2022-03-18 10:14:16 +0000

    incremented build version

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(
     name='Compendium',
-    version='0.1',
-    packages=['.compendium', 'compendium.wss'],
+    version='0.2',
+    packages=['compendium', 'compendium.wss'],
 )
